93f889f11e2fb0491b659da46f1fbadcf48fd563
Judahh <judahholanda7@gmail.com>
2019-08-31 12:01:19 -0300

    update

--- utils.ts
+++ utils.ts
@@ -1,73 +1,77 @@
 declare global {
-    interface Array<T> {
-      type: any;
-      getType(): string;
-      pushSorted(element, compareFunction): string;
-    }
-
-    interface JQueryStatic {
-      cache;
-    }
-
-    interface String {
-      replaceAll(search: string, replacement: string): string;
-    }
-  }
-
-  declare interface Array<T> {
+  interface Array<T> {
     type: any;
     getType(): string;
+    position(element, compareFunction): number;
+    pushSorted(element, compareFunction): number;
   }
 
-  declare interface JQueryStatic {
+  interface JQueryStatic {
     cache;
   }
 
-  declare interface String {
+  interface String {
     replaceAll(search: string, replacement: string): string;
   }
+}
 
-  String.prototype.replaceAll = function(search, replacement) {
-      let target = this;
-      return target.replace(new RegExp(search, 'g'), replacement);
-  };
+declare interface Array<T> {
+  type: any;
+  getType(): string;
+}
 
-  Array.prototype.getType = function () {
-    return this.type;
+declare interface JQueryStatic {
+  cache;
+}
+
+declare interface String {
+  replaceAll(search: string, replacement: string): string;
+}
+
+String.prototype.replaceAll = function (search, replacement) {
+  let target = this;
+  return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+Array.prototype.getType = function () {
+  return this.type;
+}
+
+function checkPosition(m) {
+  return ((-m - 1) >= 0) ? (-m - 1) : m;
+}
+
+Array.prototype.position = function (element, compareFunction) {
+  let m = 0;
+  let n = this.length - 1;
+  while (m <= n) {
+    let k = (n + m) >> 1;
+    if (compareFunction(element, this[k])) {
+      m = k + 1;
+    } else {
+      n = k - 1;
+    }
   }
+  return checkPosition(m);
+}
 
-  Array.prototype.pushSorted = function(element, compareFunction) {
-    this.splice((function(arr) {
-      let m = 0;
-      let n = arr.length - 1;
+Array.prototype.pushSorted = function (element, compareFunction) {
+  let index = this.position(element, compareFunction);
+  this.splice(index, 0, element);
+  return index;
+};
 
-      while (m <= n) {
-        let k = (n + m) >> 1;
-        if (compareFunction(element, arr[k])) {
-          m = k + 1;
-        } else {
-          n = k - 1;
-        }
-      }
+// interface Object {
+//     getClassName(): string;
+//     getConstructor(): any;
+// }
 
-      return ((-m - 1) >= 0) ? (-m - 1) : m;
+// Object.prototype.getConstructor = function() {
+//   return this.constructor;
+// }
 
-    })(this), 0, element);
+// Object.prototype.getClassName = function() {
+//   return this.constructor.name;
+// }
 
-    return this.length;
-  };
-
-  // interface Object {
-  //     getClassName(): string;
-  //     getConstructor(): any;
-  // }
-
-  // Object.prototype.getConstructor = function() {
-  //   return this.constructor;
-  // }
-
-  // Object.prototype.getClassName = function() {
-  //   return this.constructor.name;
-  // }
-
-  export { Array, String }
+export { Array, String }
